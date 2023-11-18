f1c344299f67053c81daf02e6a5f37fb543a7398
jxf4869 <3505888259@qq.com>
2023-11-19 00:01:28 +0800

    添加项目文件。

---
+++ winLogin.cpp
@@ -0,0 +1,34 @@
+﻿// winLogin.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MyFaceCheck.h"
+#include "winLogin.h"
+#include "afxdialogex.h"
+
+
+// winLogin 对话框
+
+IMPLEMENT_DYNAMIC(winLogin, CDialogEx)
+
+winLogin::winLogin(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MYFACECHECK_LOGIN, pParent)
+{
+
+}
+
+winLogin::~winLogin()
+{
+}
+
+void winLogin::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(winLogin, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// winLogin 消息处理程序
---
+++ winLogin.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// winLogin 对话框
+
+class winLogin : public CDialogEx
+{
+	DECLARE_DYNAMIC(winLogin)
+
+public:
+	winLogin(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~winLogin();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MYFACECHECK_LOGIN };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
